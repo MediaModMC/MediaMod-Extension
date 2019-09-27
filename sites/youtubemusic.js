7cbe43cce85de4e2805011be388539a59f0aee45
Bruno Henrique Paiva <bruno@sigane.com.br>
2019-09-27 10:38:28 -0300

    Start removing jQuery

--- sites/youtubemusic.js
+++ sites/youtubemusic.js
@@ -1,61 +1,55 @@
 setInterval(() => {
-    let title = $(".title")
-        .filter(".style-scope")
-        .filter(".ytmusic-player-bar")
-        .attr("title");
-    let data = $(".byline")
-        .filter(".style-scope")
-        .filter(".ytmusic-player-bar")
-        .attr("title");
-    let [artist, albumname, releaseDate] = data.split(" • ");
-    let albumart = $(".image")
-        .filter(".style-scope")
-        .filter(".ytmusic-player-bar")
-        .attr("src");
-    let timestampSeconds = $("#progress-bar")
-        .filter(".style-scope")
-        .filter(".ytmusic-player-bar")
-        .attr("value");
-    let lengthSeconds = $("#progress-bar")
-        .filter(".style-scope")
-        .filter(".ytmusic-player-bar")
-        .attr("aria-valuemax");
-    let timestamp = timestampSeconds * 1000;
-    length = lengthSeconds * 1000;
+  const title = document
+    .querySelector(".title.style-scope.ytmusic-player-bar")
+    .getAttribute("title");
+  const [artist, albumname, releaseDate] = document
+    .querySelector(".byline.style-scope.ytmusic-player-bar")
+    .getAttribute("title")
+    .split(" • ");
+  const albumArt = document
+    .querySelector("img.image.style-scope.ytmusic-player-bar")
+    .getAttribute("src");
 
-    if (title === "") {
-        return;
+  const progressBarElement = document.querySelector(
+    "#progress-bar.style-scope.ytmusic-player-bar"
+  );
+  const progress = Number(progressBarElement.getAttribute("value")) * 1000;
+  const duration =
+    Number(progressBarElement.getAttribute("aria-valuemax")) * 1000;
+
+  if (!title || title.trim().length === 0) {
+    return;
+  }
+
+  const data = {
+    progress_ms: progress,
+    item: {
+      album: {
+        artists: [
+          {
+            name: artist
+          }
+        ],
+        images: [
+          {
+            url: albumArt
+          }
+        ]
+      },
+      duration_ms: duration,
+      name: title
     }
+  };
 
-    let json = {
-        progress_ms: timestamp,
-        item: {
-            album: {
-                artists: [
-                    {
-                        name: artist
-                    }
-                ],
-                images: [
-                    {
-                        url: albumart
-                    }
-                ]
-            },
-            duration_ms: length,
-            name: title
-        }
-    };
-
-    fetch("http://localhost:9102/", {
-        method: "post",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(json)
-    })
-        .then(response => console.log(response))
-        .catch(error =>
-            console.error("[MediaMod] Error when sending request", error)
-        );
+  fetch("http://localhost:9102/", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => console.log(response))
+    .catch(error =>
+      console.error("[MediaMod] Error when sending request", error)
+    );
 }, 3000);
